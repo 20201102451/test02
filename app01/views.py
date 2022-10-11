66f4d5e61468c00bc05b6b8505134bac508899ab
ShiSL <446294034@qq.com>
2022-10-11 13:02:53 +0800

    first commit by ShiSL

--- app01/views.py
+++ app01/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("这是首页")
+
+def index3(request):
+    return HttpResponse("这是SSL首页")
