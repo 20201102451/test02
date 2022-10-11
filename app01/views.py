0adcb9f0b8fa7af6657c89702c77949db2c660a2
AC-Asteroid <92983460+AC-Asteroid@users.noreply.github.com>
2022-10-11 20:12:01 +0800

    修改后提交

--- app01/views.py
+++ app01/views.py
@@ -3,14 +3,12 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("这是首页")
-<<<<<<< HEAD
 
 def index3(request):
     return HttpResponse("这是SSL首页")
-=======
+
 def index2(request):
     return HttpResponse("这是测试首页")
->>>>>>> origin/master
 
 def indexFan(request):
     return HttpResponse("这是提交一")
